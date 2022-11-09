cd012c9d160b878aad1ad38d2d3eb85055675caf
tony <linchiah01@gmail.com>
2022-11-09 13:54:34 +0800

    Initial Commit

---
+++ lin_HW3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  lin_HW3
+//
+//  Created by User01 on 2022/11/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ lin_HW3/lin_HW3App.swift
@@ -0,0 +1,17 @@
+//
+//  lin_HW3App.swift
+//  lin_HW3
+//
+//  Created by User01 on 2022/11/9.
+//
+
+import SwiftUI
+
+@main
+struct lin_HW3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
